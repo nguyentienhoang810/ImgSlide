3fc083fe2300993a68a57ca834637df431ebfcdf
Nguyen Tien Hoang <nguyentienhoang810@gmail.com>
2019-01-23 08:20:01 +0700

    Update ViewController.swift

--- ImgSlide/ViewController.swift
+++ ImgSlide/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let imgFrame = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width))
+    private let imgFrame = UIView(frame: CGRect(x: 0, y: 100, width: UIScreen.main.bounds.width, height: 2))
     private let imgView = UIImageView()
+    private let line = UIView()
     private let nextBtn = UIButton()
     private let imgArr = ["1", "2", "3"]
     private var imgIndex = 1
@@ -22,8 +23,8 @@ class ViewController: UIViewController {
         view.addSubview(imgFrame)
         
         imgView.image = UIImage(named: "1")
-        imgFrame.addSubview(imgView)
-        imgView.frame = imgFrame.frame
+        imgView.frame = CGRect(x: 0, y: 70, width: 30, height: 30)
+        view.addSubview(imgView)
         
         view.addSubview(nextBtn)
         nextBtn.frame = CGRect(origin: CGPoint(x: view.center.x, y: UIScreen.main.bounds.width + 30), size: CGSize(width: 100, height: 30))
@@ -42,10 +43,10 @@ class ViewController: UIViewController {
     
     private func addImgSlide(ingIndex: Int) {
         UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseOut, animations: {
-            self.imgView.frame.origin.x += 500 //di chuyển imgView qua phải 500.
-        }) { _ in
-            self.imgView.frame.origin.x -= 500 //Sau khi di chuyển qua phải 500 phải đưa imgVIew về vị trí cũ (di duyển qua trái 500).
             self.imgView.image = UIImage(named: "\(self.imgIndex)")
+            self.imgView.frame.origin.x += UIScreen.main.bounds.width / 10
+        }) { _ in
+            
         }
     }
 }
